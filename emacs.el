e125ccede496a1da15f0bfdd99da1c8198eb58ce
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-04-01 16:44:44 +0200

    refactor emacs config

--- emacs.el
+++ emacs.el
@@ -1,83 +1,22 @@
-; remove Toolbar
+; disable Toolbar
 (tool-bar-mode -1)
-; remove Menubar
+; disable Menubar
 (menu-bar-mode -1)
-; remove Scrollbar
+; disable Scrollbar
 (scroll-bar-mode -1)
-; remove StartupScreen
+; disable StartupScreen
 (setq inhibit-startup-screen t)
 
-; line numbers
-(line-number-mode t)
-; column number in Modeline
-(column-number-mode t)
-; line number on the left
+; show line numbers
 (global-linum-mode t)
-; line numbers separation
 (setq linum-format "%d ")
 ; highlight current line
 (global-hl-line-mode 1)
-; show matching parenthesis
+(set-face-background 'hl-line "#333333")
+; highlight matching parenthesis
 (show-paren-mode 1)
 
-; Source Code Pro
-(set-default-font "Source Code Pro Light 14")
-; set correct font in GUI with emacs server
-(setq default-frame-alist '((font . "Source Code Pro Light 14")))
-
-; Zenburn color
-; $ git clone https://github.com/bbatsov/zenburn-emacs.git
-(add-to-list 'custom-theme-load-path "~/.emacs.d/zenburn-emacs")
-(load-theme 'zenburn t)
-
-; WiteSpace mode
-(global-whitespace-mode t)
-(global-whitespace-toggle-options t)
-; 80 columns
-(setq-default whitespace-line-column 80)
-; tabs, trailing white spaces
-(setq-default whitespace-style '(face tab-mark trailing lines-tail empty))
-; delete trailing white spaces
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-; indentation
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq indent-line-function 'insert-tab)
-; C/C++ indentation
-(setq c-basic-offset 2)
-; bash indentation
-(setq sh-basic-offset 2)
-; elisp indentation
-(setq lisp-indent-offset 2)
-
-; Rainbow delimiters mode
-; $ git clone https://github.com/Fanael/rainbow-delimiters.git
-(add-to-list 'load-path "~/.emacs.d/rainbow-delimiters")
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-
-; Smex mode
-; $ git clone https://github.com/nonsequitur/smex.git
-;(add-to-list 'load-path "~/.emacs.d/smex")
-;(require 'smex)
-;(smex-initialize)
-;(global-set-key (kbd "M-x") 'smex)
-;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-
-; Helm mode
-; $ git clone https://github.com/jwiegley/emacs-async.git
-; $ git clone https://github.com/emacs-helm/helm.git
-;(add-to-list 'load-path "~/.emacs.d/emacs-async")
-;(add-to-list 'load-path "~/.emacs.d/helm")
-;(require 'helm-config)
-;(helm-mode 1)
-
-; RecentFiles mode
-(recentf-mode 1)
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
-
-; terminal copy/paste to/from clipboard
+; terminal clipboard yank/paste
 ; $ sudo apt-get install xsel
 (setq x-select-enable-clipboard t)
 (unless window-system
@@ -94,27 +33,43 @@
   (setq interprogram-cut-function 'xsel-cut-function)
   (setq interprogram-paste-function 'xsel-paste-function)))
 
-; XML format
-; $ sudo apt-get install libxml2-utils
-; M-x xml-format RET -> format XML
-(defun xml-format ()
-  (interactive)
-  (save-excursion
-    (shell-command-on-region (mark) (point) "xmllint --format -"
-      (buffer-name) t)))
-
-; PostgreSQL mode
-(add-to-list 'auto-mode-alist '("\\.sql\\'" . (lambda () (sql-mode)
-  (sql-highlight-postgres-keywords))))
-
-; TeX mode
+; WiteSpace mode
+(require 'whitespace)
+(global-whitespace-mode t)
+(global-whitespace-toggle-options t)
+; 80 columns margin
+(setq-default whitespace-line-column 80)
 ; M-q -> format selected text
 (setq-default fill-column 80)
+; show tab marks, trailing blanks, long lines
+(setq-default whitespace-style '(face tab-mark trailing lines-tail))
+; remove trailing blanks on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-; Ninja mode
-; $ git clone https://github.com/martine/ninja.git
-(add-to-list 'load-path "~/.emacs.d/ninja")
-(require 'ninja-mode)
+; indentation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq indent-line-function 'insert-tab)
+; Emacs Lisp indentation
+(setq-default lisp-indent-offset 2)
+; Bash indentation
+(setq-default sh-basic-offset 2)
+; C indentation
+(setq-default c-basic-offset 2)
+
+; Source Code Pro font
+(set-frame-font "Source Code Pro Light 16")
+
+; Zenburn color
+; $ git clone https://github.com/bbatsov/zenburn-emacs.git
+(add-to-list 'custom-theme-load-path "~/.emacs.d/zenburn-emacs")
+(load-theme 'zenburn t)
+
+; Rainbow delimiters mode
+; $ git clone https://github.com/Fanael/rainbow-delimiters.git
+(add-to-list 'load-path "~/.emacs.d/rainbow-delimiters")
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 ; JS2 mode
 ; $ git clone https://github.com/mooz/js2-mode.git
@@ -128,7 +83,7 @@
 ; Web mode
 ; $ git clone https://github.com/fxbois/web-mode.git
 (add-to-list 'load-path "~/.emacs.d/web-mode")
-(require 'web-mode)
+(autoload 'web-mode "web-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2)
@@ -140,48 +95,81 @@
 ; $ git clone https://github.com/smihica/emmet-mode.git
 ; C-j -> expand line
 (add-to-list 'load-path "~/.emacs.d/emmet-mode")
-(require 'emmet-mode)
+(autoload 'emmet-mode "emmet-mode" nil t)
 (add-hook 'web-mode-hook 'emmet-mode)
 
-; Jade mode
-; $ git clone https://github.com/brianc/jade-mode.git
-(add-to-list 'load-path "~/.emacs.d/jade-mode")
-(require 'jade-mode)
-(require 'sws-mode)
-(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
-(add-to-list 'auto-mode-alist '("\\.styl\\'" . sws-mode))
-
-; Markdown mode
-; $ git clone https://github.com/defunkt/markdown-mode.git
-(add-to-list 'load-path "~/.emacs.d/markdown-mode")
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-; Cucumber mode
-; $ git clone https://github.com/michaelklishin/cucumber.el.git
-(add-to-list 'load-path "~/.emacs.d/cucumber.el")
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\\.feature\\'" . feature-mode))
-
-; Clojure mode
-; $ git clone https://github.com/clojure-emacs/clojure-mode.git
-(add-to-list 'load-path "~/.emacs.d/clojure-mode")
-(require 'clojure-mode)
-
-; YASnippet mode
-; $ git clone https://github.com/capitaomorte/yasnippet.git
-(add-to-list 'load-path "~/.emacs.d/yasnippet")
-(require 'yasnippet)
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-(yas-global-mode 1)
+;; ; Smex mode
+;; ; $ git clone https://github.com/nonsequitur/smex.git
+;; ;(add-to-list 'load-path "~/.emacs.d/smex")
+;; ;(require 'smex)
+;; ;(smex-initialize)
+;; ;(global-set-key (kbd "M-x") 'smex)
+;; ;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;;
+;; ; Helm mode
+;; ; $ git clone https://github.com/jwiegley/emacs-async.git
+;; ; $ git clone https://github.com/emacs-helm/helm.git
+;; ;(add-to-list 'load-path "~/.emacs.d/emacs-async")
+;; ;(add-to-list 'load-path "~/.emacs.d/helm")
+;; ;(require 'helm-config)
+;; ;(helm-mode 1)
+;;
+;; ; XML format
+;; ; $ sudo apt-get install libxml2-utils
+;; ; M-x xml-format RET -> format XML
+;; (defun xml-format ()
+;;   (interactive)
+;;   (save-excursion
+;;     (shell-command-on-region (mark) (point) "xmllint --format -"
+;;       (buffer-name) t)))
+;;
+;; ; PostgreSQL mode
+;; (add-to-list 'auto-mode-alist '("\\.sql\\'" . (lambda () (sql-mode)
+;;   (sql-highlight-postgres-keywords))))
+;;
+;; ; Ninja mode
+;; ; $ git clone https://github.com/martine/ninja.git
+;; (add-to-list 'load-path "~/.emacs.d/ninja")
+;; (require 'ninja-mode)
+;;
+;; ; Jade mode
+;; ; $ git clone https://github.com/brianc/jade-mode.git
+;; (add-to-list 'load-path "~/.emacs.d/jade-mode")
+;; (require 'jade-mode)
+;; (require 'sws-mode)
+;; (add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
+;; (add-to-list 'auto-mode-alist '("\\.styl\\'" . sws-mode))
+;;
+;; ; Markdown mode
+;; ; $ git clone https://github.com/defunkt/markdown-mode.git
+;; (add-to-list 'load-path "~/.emacs.d/markdown-mode")
+;; (autoload 'markdown-mode "markdown-mode"
+;;   "Major mode for editing Markdown files" t)
+;; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;;
+;; ; Cucumber mode
+;; ; $ git clone https://github.com/michaelklishin/cucumber.el.git
+;; (add-to-list 'load-path "~/.emacs.d/cucumber.el")
+;; (require 'feature-mode)
+;; (add-to-list 'auto-mode-alist '("\\.feature\\'" . feature-mode))
+;;
+;; ; Clojure mode
+;; ; $ git clone https://github.com/clojure-emacs/clojure-mode.git
+;; (add-to-list 'load-path "~/.emacs.d/clojure-mode")
+;; (require 'clojure-mode)
+;;
+;; ; YASnippet mode
+;; ; $ git clone https://github.com/capitaomorte/yasnippet.git
+;; (add-to-list 'load-path "~/.emacs.d/yasnippet")
+;; (require 'yasnippet)
+;; (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+;; (yas-global-mode 1)
 
 ; Evil mode
 ; $ git clone https://gitorious.org/evil/evil.git
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
-;(evil-ex-define-cmd "ls" 'ibuffer)
 
 ; Spell mode
 ; M-x ispell
--- update.sh
+++ update.sh
@@ -1,51 +1,61 @@
-function updateRepo {
+function updateGit {
   echo [ $1 ];
   cd ~/.emacs.d;
   if cd $1; then git pull; else git clone $2; fi
 }
 
-function updateRepoHG {
+function updateHg {
   echo [ $1 ];
   cd ~/.emacs.d;
   if cd $1; then hg pull -u; else hg clone $2; fi
 }
 
-#cd ~/.emacs.d && emacs --batch -f batch-byte-compile emacs.el
+function compileEmacs {
+  cd ~/.emacs.d/$1;
+  for file in *.el; do
+    if [[ -f $file ]] && [[ ! $file =~ test ]]; then
+      emacs --batch -f batch-byte-compile $file;
+    fi
+  done
+}
 
-updateRepo 'zenburn-emacs' 'https://github.com/bbatsov/zenburn-emacs.git'
-updateRepo 'rainbow-delimiters' 'https://github.com/Fanael/rainbow-delimiters.git'
-updateRepo 'smex' 'https://github.com/nonsequitur/smex.git'
-updateRepo 'emacs-async' 'https://github.com/jwiegley/emacs-async.git' \
-  && cd ~/.emacs.d/emacs-async \
-  && emacs --batch -f batch-byte-compile async.el
-updateRepo 'helm' 'https://github.com/emacs-helm/helm.git' \
-  && cd ~/.emacs.d/helm && make
+#updateGit 'zenburn-emacs' 'https://github.com/bbatsov/zenburn-emacs.git'
+#  compileEmacs 'zenburn-emacs'
+#updateGit 'rainbow-delimiters' 'https://github.com/Fanael/rainbow-delimiters.git'
+#  compileEmacs 'rainbow-delimiters'
 
-updateRepo 'ninja' 'https://github.com/martine/ninja.git' \
-  && cd ~/.emacs.d/ninja && cp misc/ninja-mode.el . \
-  && emacs --batch -f batch-byte-compile ninja-mode.el
-updateRepo 'js2-mode' 'https://github.com/mooz/js2-mode.git' \
-  && cd ~/.emacs.d/js2-mode && make
-updateRepo 'web-mode' 'https://github.com/fxbois/web-mode.git' \
-  && cd ~/.emacs.d/web-mode \
-  && emacs --batch -f batch-byte-compile web-mode.el
-updateRepo 'emmet-mode' 'https://github.com/smihica/emmet-mode.git' \
-  && cd ~/.emacs.d/emmet-mode \
-  && emacs --batch -f batch-byte-compile emmet-mode.el
-updateRepo 'jade-mode' 'https://github.com/brianc/jade-mode.git' \
-  && cd ~/.emacs.d/jade-mode \
-  && emacs --batch -f batch-byte-compile jade-mode.el
-updateRepo 'markdown-mode' 'https://github.com/defunkt/markdown-mode.git' \
-  && cd ~/.emacs.d/markdown-mode \
-  && emacs --batch -f batch-byte-compile markdown-mode.el
-updateRepo 'cucumber.el' 'https://github.com/michaelklishin/cucumber.el.git' \
-  && cd ~/.emacs.d/cucumber.el \
-  && emacs --batch -f batch-byte-compile feature-mode.el
-updateRepo 'clojure-mode' 'https://github.com/clojure-emacs/clojure-mode.git' \
-  && cd ~/.emacs.d/clojure-mode && make
+#updateGit 'js2-mode' 'https://github.com/mooz/js2-mode.git'
+#  cd ~/.emacs.d/js2-mode && make
+#updateGit 'web-mode' 'https://github.com/fxbois/web-mode.git'
+#  compileEmacs 'web-mode'
+#updateGit 'emmet-mode' 'https://github.com/smihica/emmet-mode.git'
+#  compileEmacs 'emmet-mode'
 
-updateRepo 'yasnippet' '--recursive https://github.com/capitaomorte/yasnippet.git' \
-  && cd ~/.emacs.d/yasnippet \
-  && emacs --batch -f batch-byte-compile yasnippet.el
-updateRepoHG 'evil' 'https://bitbucket.org/lyro/evil' \
-  && cd ~/.emacs.d/evil && make
+# updateGit 'smex' 'https://github.com/nonsequitur/smex.git'
+# updateGit 'emacs-async' 'https://github.com/jwiegley/emacs-async.git' \
+#   && cd ~/.emacs.d/emacs-async \
+#   && emacs --batch -f batch-byte-compile async.el
+# updateGit 'helm' 'https://github.com/emacs-helm/helm.git' \
+#   && cd ~/.emacs.d/helm && make
+#
+# updateGit 'ninja' 'https://github.com/martine/ninja.git' \
+#   && cd ~/.emacs.d/ninja && cp misc/ninja-mode.el . \
+#   && emacs --batch -f batch-byte-compile ninja-mode.el
+# updateGit 'jade-mode' 'https://github.com/brianc/jade-mode.git' \
+#   && cd ~/.emacs.d/jade-mode \
+#   && emacs --batch -f batch-byte-compile jade-mode.el
+# updateGit 'markdown-mode' 'https://github.com/defunkt/markdown-mode.git' \
+#   && cd ~/.emacs.d/markdown-mode \
+#   && emacs --batch -f batch-byte-compile markdown-mode.el
+# updateGit 'cucumber.el' 'https://github.com/michaelklishin/cucumber.el.git' \
+#   && cd ~/.emacs.d/cucumber.el \
+#   && emacs --batch -f batch-byte-compile feature-mode.el
+# updateGit 'clojure-mode' 'https://github.com/clojure-emacs/clojure-mode.git' \
+#   && cd ~/.emacs.d/clojure-mode && make
+#
+# updateGit 'yasnippet' '--recursive https://github.com/capitaomorte/yasnippet.git' \
+#   && cd ~/.emacs.d/yasnippet \
+#   && emacs --batch -f batch-byte-compile yasnippet.el
+
+#updateHg 'evil' 'https://bitbucket.org/lyro/evil'
+#  cd ~/.emacs.d/evil && make
