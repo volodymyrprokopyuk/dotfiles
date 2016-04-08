b3a51718a19f8d5cd8b92103d4a68764c4e1f02a
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-04-08 16:06:16 +0200

    add git config

--- .bash.sh
+++ .bash.sh
@@ -40,6 +40,9 @@ man() {
 # install Git
 [ -s "$HOME/local/bin/git-completion.bash" ] \
   && source "$HOME/local/bin/git-completion.bash"
+git config --global user.name 'Volodymyr Prokopyuk'
+git config --global user.email 'volodymyrprokopyuk@gmail.com'
+git config --global core.editor 'em -nw'
 git config --global alias.lg "log --all --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 # install NVM
 export NVM_DIR="$HOME/.nvm"
