9283740ffe705c69050a87fad34dd8038725f1b6
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-10-05 12:46:16 +0200

    feat(emacs): maximize emacs window on startup

--- roles/emacs-config/files/emacs.el
+++ roles/emacs-config/files/emacs.el
@@ -1,11 +1,13 @@
-; disable Toolbar
+; disable toolbar
 (tool-bar-mode -1)
-; disable Menubar
+; disable menubar
 (menu-bar-mode -1)
-; disable Scrollbar
+; disable scrollbar
 (scroll-bar-mode -1)
-; disable Startup Screen
+; disable startup screen
 (setq inhibit-startup-screen t)
+; maximize emacs window on startup
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 ; set calendar week start day to Monday
 (add-hook 'calendar-mode-hook '(lambda () (setq calendar-week-start-day 1)))
 
@@ -55,7 +57,6 @@
 (add-hook 'text-mode-hook 'auto-fill-mode)
 
 ; indentation
-; C-c C-s => show offset variable
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
@@ -66,31 +67,31 @@
 ; C indentation
 (setq-default c-basic-offset 4)
 
-; Source Code Pro Font
+; Source Code Pro font
 (set-frame-font "Source Code Pro Light 14")
 
-; Common Extensions
+; common extensions
 (add-to-list 'load-path "~/.emacs.d/epl")
 (add-to-list 'load-path "~/.emacs.d/pkg-info.el")
 (add-to-list 'load-path "~/.emacs.d/s.el")
 (add-to-list 'load-path "~/.emacs.d/dash.el")
 (add-to-list 'load-path "~/.emacs.d/emacs-async")
 
-; Add $PATH to exec-path
+; add $PATH to exec-path
 (add-to-list 'load-path "~/.emacs.d/exec-path-from-shell")
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
-; Zenburn Theme (done)
+; Zenburn theme (done)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/zenburn-emacs")
 (load-theme 'zenburn t)
 
-; Rainbow Delimiters Mode (done)
+; Rainbow Delimiters mode (done)
 (add-to-list 'load-path "~/.emacs.d/rainbow-delimiters")
 (autoload 'rainbow-delimiters-mode "rainbow-delimiters" nil t)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-; Powerline/Spaceline Modeline (done)
+; Powerline/Spaceline modeline (done)
 (add-to-list 'load-path "~/.emacs.d/powerline")
 (add-to-list 'load-path "~/.emacs.d/spaceline")
 (require 'spaceline-config)
@@ -100,13 +101,13 @@
 (setq powerline-default-separator 'wave)
 (spaceline-compile)
 
-; Smartparens Mode
+; Smartparens mode
 (add-to-list 'load-path "~/.emacs.d/smartparens")
 (require 'smartparens-config)
 (add-hook 'prog-mode-hook 'smartparens-mode)
 (add-hook 'text-mode-hook 'smartparens-mode)
 
-; Helm Mode
+; Helm mode
 (add-to-list 'load-path "~/.emacs.d/helm")
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -114,7 +115,7 @@
 (global-set-key (kbd "M-s o") 'helm-occur)
 (helm-mode 1)
 
-; Helm Ag Mode (done)
+; Helm Ag mode (done)
 (add-to-list 'load-path "~/.emacs.d/emacs-helm-ag")
 (require 'helm-ag)
 (custom-set-variables
@@ -122,18 +123,18 @@
     '(helm-ag-command-option "--hidden --ignore *~ --ignore .git"))
 (global-set-key (kbd "M-s s") 'helm-do-ag-project-root)
 
-; Company Mode
+; Company mode
 (add-to-list 'load-path "~/.emacs.d/company-mode")
 (require 'company)
 (global-set-key (kbd "M-s SPC") 'company-complete)
 (add-hook 'after-init-hook 'global-company-mode)
 
-; Avy Mode
+; Avy mode
 (add-to-list 'load-path "~/.emacs.d/avy")
 (require 'avy)
 (global-set-key (kbd "M-s j") 'avy-goto-char-timer)
 
-; Magit Mode
+; Magit mode
 (add-to-list 'load-path "~/.emacs.d/with-editor")
 (add-to-list 'load-path "~/.emacs.d/magit/lisp")
 (load "~/.emacs.d/magit/lisp/magit-autoloads.el")
@@ -142,10 +143,10 @@
     (add-to-list 'Info-directory-list "~/.emacs.d/magit/Documentation/"))
 (global-set-key (kbd "C-x g") 'magit-status)
 
-; Emacs Lisp Mode
+; Emacs Lisp mode
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
-; JS2 Mode
+; JS2 mode
 (add-to-list 'load-path "~/.emacs.d/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
@@ -153,7 +154,7 @@
 (setq-default js2-basic-offset 4)
 (setq-default js-indent-level 4)
 
-; Web Mode
+; Web mode
 (add-to-list 'load-path "~/.emacs.d/web-mode")
 (autoload 'web-mode "web-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
@@ -165,31 +166,31 @@
         (setq web-mode-css-indent-offset 4)
         (setq web-mode-code-indent-offset 4)))
 
-; Emmet Mode
+; Emmet mode
 ; C-j => expand line
 (add-to-list 'load-path "~/.emacs.d/emmet-mode")
 (autoload 'emmet-mode "emmet-mode" nil t)
 (add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'nxml-mode-hook 'emmet-mode)
 
-; Markdown Mode
+; Markdown mode
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
 (autoload 'markdown-mode "markdown-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-; YAML Mode
+; YAML mode
 (add-to-list 'load-path "~/.emacs.d/yaml-mode")
 (autoload 'yaml-mode "yaml-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 
-; Evil Mode
+; Evil mode
 (add-to-list 'load-path "~/.emacs.d/goto-chg.el")
 (require 'goto-chg)
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
-; Ibuffer Mode
+; Ibuffer mode
 (evil-ex-define-cmd "ls" 'ibuffer)
 (evil-set-initial-state 'ibuffer-mode 'normal)
