9fbf3e76de102469e71ec970cec0ecbb5b870f67
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-12-06 22:40:01 +0100

    feat(emacs): add Evil Nerd Commenter

--- roles/emacs-config/files/emacs.el
+++ roles/emacs-config/files/emacs.el
@@ -247,3 +247,8 @@
 (add-to-list 'load-path "~/.emacs.d/evil-surround")
 (require 'evil-surround)
 (global-evil-surround-mode 1)
+
+; Evil Nerd Commenter
+(add-to-list 'load-path "~/.emacs.d/evil-nerd-commenter")
+(require 'evil-nerd-commenter)
+(evilnc-default-hotkeys)
