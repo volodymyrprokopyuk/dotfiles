5076df360a4e86d7c11de7cc6d30bc4fd81063f6
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2018-07-04 14:34:59 +0200

    feat(docker): add wait_for_port shell function

--- docker-stack/sqlserver/deploy.sh
+++ docker-stack/sqlserver/deploy.sh
@@ -2,11 +2,23 @@
 
 set -eu
 
-docker stack deploy -c docker-compose.yml sqlserver
+wait_for_port() {
+    local host=${1:?"ERROR: host is not defined"}
+    local port=${2:?"ERROR: port is not defined"}
+    local timeout=${3:-10}
+    while ! nc -z $host $port; do
+        sleep 1
+        ((timeout--))
+        if [[ $timeout -eq 0 ]]; then
+            echo "ERROR: conainer has not started correctly"
+            exit 1
+        fi
+    done
+}
 
-while ! nc -z localhost 1433; do
-    sleep 1
-done
+docker stack deploy -c docker-compose.yml sqlserver
+wait_for_port localhost 1433
+
 CONTAINER_ID=$(docker ps --filter "name=sqlserver" --quiet)
 echo "CONTAINER_ID=$CONTAINER_ID"
 
@@ -15,10 +27,6 @@ docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 
 echo "Creating database schema"
 docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S localhost -U family -P 'Password!1' -d people -i /home/002_create_schema.sql
 
-if [[ -n $CONTAINER_ID ]]; then
-    docker logs -f $CONTAINER_ID
-else
-    echo "ERROR: conainer has not started correctly"
-fi
+docker logs -f $CONTAINER_ID
 
 # docker stack rm sqlserver
