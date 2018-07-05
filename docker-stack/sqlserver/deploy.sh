006b3131db075aa2470cd04df34246801d1af3c9
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2018-07-05 09:11:07 +0200

    refactor(docker): add HOST, PORT and TIMEOUT variables

--- docker-stack/sqlserver/deploy.sh
+++ docker-stack/sqlserver/deploy.sh
@@ -2,10 +2,14 @@
 
 set -eu
 
+HOST=localhost
+PORT=1433
+TIMEOUT=10
+
 wait_for_port() {
     local host=${1:?"ERROR: host is not defined"}
     local port=${2:?"ERROR: port is not defined"}
-    local timeout=${3:-10}
+    local timeout=${3:-$TIMEOUT}
     while ! nc -z $host $port; do
         sleep 1
         (( timeout-- ))
@@ -17,17 +21,17 @@ wait_for_port() {
 }
 
 docker stack deploy -c docker-compose.yml sqlserver
-wait_for_port localhost 1433
+wait_for_port $HOST $PORT
 
 CONTAINER_ID=$(docker ps --filter "name=sqlserver" --quiet)
 echo "CONTAINER_ID=$CONTAINER_ID"
 
 echo "Creating database and users with permissions"
-docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Password1!' -d master -t 10 -i /home/001_create_database_and_users.sql
+docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S $HOST -U sa -P 'Password1!' -d master -t $TIMEOUT -i /home/001_create_database_and_users.sql
 echo "Creating database schema"
-docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S localhost -U family_ddl -P 'Password1!' -d people -t 10 -i /home/002_create_schema.sql
+docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S $HOST -U family_ddl -P 'Password1!' -d people -t $TIMEOUT -i /home/002_create_schema.sql
 echo "Testing DML"
-docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S localhost -U family_dml -P 'Password1!' -d people -t 10 -i /home/003_test_dml.sql
+docker exec -it $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd -S $HOST -U family_dml -P 'Password1!' -d people -t $TIMEOUT -i /home/003_test_dml.sql
 
 docker logs -f $CONTAINER_ID
 
