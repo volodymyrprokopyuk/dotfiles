25caa7afff70aa9f44a1cabef8887c596da8bd9d
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-11-24 19:23:13 +0100

    feat(git): add git cb alias

--- upgrade.sh
+++ upgrade.sh
@@ -161,12 +161,13 @@ function git_config_aliases {
     git config --global alias.lf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\n"'
     git config --global alias.lsf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\n" -S'
     git config --global alias.lgf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\n" -G'
+    git config --global alias.bb "branch -a -vv"
     git config --global alias.bm "branch --merged"
     git config --global alias.bnm "branch --no-merged"
-    git config --global alias.bb "branch -a -vv"
+    git config --global alias.ch "checkout"
+    git config --global alias.cb '!git ch "$(git branch | sed "s/  //" | grep -v "^*" | fzf)"'
     git config --global alias.a "!git add -A && git s"
     git config --global alias.cm "!git dch && git commit"
-    git config --global alias.ch "checkout"
 }
 
 function git_config_diff {
