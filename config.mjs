0064c10ab449ff7cfc5711dc26ea01a6af4661ba
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-05-27 12:15:22 +0200

    feat(config): add Node.js-based config

---
+++ config.mjs
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+import { promisify } from "util"
+import { exec as execCb } from "child_process"
+const exec = promisify(execCb)
+import { existsSync } from "fs"
+import { mkdir, copyFile, writeFile, unlink } from "fs/promises"
+import { homedir } from "os"
+const home = homedir()
+
+async function removeFile(file) {
+  if (existsSync(file)) { await unlink(file) }
+}
+
+async function git() {
+  const cmds = `git config --global user.name "Volodymyr Prokopyuk"
+    git config --global user.email "volodymyrprokopyuk@gmail.com"
+    git config --global core.excludesfile "~/.gitignore"
+    git config --global init.defaultBranch "master"
+    git config --global push.default "simple"
+    git config --global pull.rebase false
+    git config --global diff.algorithm "histogram"
+    git config --global status.showUntrackedFiles "all"
+    git config --global credential.helper "cache --timeout=86400"
+    git config --global core.quotepath "false"
+    git config --global core.pager "delta"
+    git config --global delta.syntax-theme "OneHalfDark"
+    git config --global alias.s "!git status -sb && git stash list"
+    git config --global alias.d "diff"
+    git config --global alias.ds "diff --stat"
+    git config --global alias.dc "diff --cached"
+    git config --global alias.dcs "diff --cached --stat"
+    git config --global alias.dch "diff --check"
+    git config --global alias.l 'log --all --graph --abbrev-commit --pretty=format:"%C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)"'
+    git config --global alias.lf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\\n"'
+    git config --global alias.lsf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\\n" -S'
+    git config --global alias.lgf 'log -p -M --follow --abbrev-commit --pretty=format:"%C(bold blue)commit%C(reset) %C(red)%h%C(reset) -%C(bold yellow)%d%C(reset) %C(yellow)%s%C(reset) %C(green)<%an>%C(reset) %C(blue)(%cr)%C(reset)\\n" -G'
+    git config --global alias.bb "branch -a -vv"
+    git config --global alias.bm "branch --merged"
+    git config --global alias.bnm "branch --no-merged"
+    git config --global alias.ch "checkout"
+    git config --global alias.a "!git add -A && git s"
+    git config --global alias.cm "!git dch && git commit"
+  `
+  await removeFile(`${home}/.gitconfig`)
+  for (const cmd of cmds.split("\n")) { await exec(cmd) }
+  const gitignore = ["*~", "node_modules"]
+  await writeFile(`${home}/.gitignore`, gitignore.join("\n"))
+}
+
+async function tmux() {
+  await copyFile(".tmux.conf", `${home}/.tmux.conf`)
+}
+
+async function zsh() {
+  await copyFile(".zshrc", `${home}/.zshrc`)
+}
+
+async function emacs() {
+  await mkdir(`${home}/.doom.d`, { recursive: true })
+  for (const file of ["packages.el", "init.el", "config.el"]) {
+    await copyFile(file, `${home}/.doom.d/${file}`)
+  }
+}
+
+async function psql() {
+  await copyFile(".psqlrc", `${home}/.psqlrc`)
+}
+
+for (const arg of process.argv.slice(2)) {
+  switch (arg) {
+    case "git": await git(); break
+    case "tmux": await tmux(); break
+    case "zsh": await zsh(); break
+    case "emacs": await emacs(); break
+    case "psql": await psql(); break
+    case "all":
+      await Promise.all([git(), tmux(), zsh(), emacs(), psql()]); break
+    default: error(`ERROR: unknown option ${arg}`)
+  }
+}
