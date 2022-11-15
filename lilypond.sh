88f0d9135fd5b40bc0a6dabfff1d0f07f32fe1b7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-11-15 10:33:10 +0100

    feat(lilypond): upgreade installation

--- lilypond.sh
+++ lilypond.sh
@@ -7,11 +7,12 @@ set -eu
 
 LP_HOME=$HOME/.lilypond
 LP_URL=https://lilypond.org/download/source/v2.23
-LP_VERSION=2.23.14
+LP_VERSION=2.23.81
 
 mkdir -p $LP_HOME && cd $LP_HOME
 curl $LP_URL/lilypond-$LP_VERSION.tar.gz | tar -xz
 cd lilypond-$LP_VERSION && mkdir -p build && cd build
 ../autogen.sh --noconfigure \
-  && ../configure --prefix=$LP_HOME --disable-documentation
+  && ../configure --prefix=$LP_HOME --disable-documentation \
+  GUILE_FLAVOR=guile-3.0
 make -j4 all && make install
