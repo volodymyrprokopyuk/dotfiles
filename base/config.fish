214a4248b358c337fa312c6a31f3679f56d8b843
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-10-29 11:44:44 +0100

    feat(fish): add custom theme

--- base/config.fish
+++ base/config.fish
@@ -5,7 +5,29 @@ set -gx LESS -RF
 set -gx LS_COLORS (vivid generate snazzy)
 set -gx BAT_STYLE plain
 set -gx BAT_THEME 1337
-set -U fish_greeting
+
+set -g fish_greeting
+
+set -g fish_color_normal D7E8FE
+set -g fish_color_error FF6C6C
+set -g fish_color_cancel FF7F56
+set -g fish_color_autosuggestion BABAFF
+
+set -g fish_color_command FFF467 --bold
+set -g fish_color_option FFAF3B
+set -g fish_color_param FFED95
+
+set -g fish_color_keyword FFFFFF --bold
+set -g fish_color_quote FF8C62
+set -g fish_color_valid_path 61FDA5 --underline
+
+set -g fish_color_operator 7DCEFA
+set -g fish_color_redirection 79FB6B
+set -g fish_color_end 69F7F7
+
+set -g fish_color_comment 00B567 --italics
+set -g fish_color_escape 23A700
+
 bind \em accept-autosuggestion
 
 starship init fish | source
