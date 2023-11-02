e00456a5df336dd169d7f27a80569c89109fab88
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-11-02 21:36:19 +0100

    chore(fish): remove vi mode

--- base/config.fish
+++ base/config.fish
@@ -32,13 +32,6 @@ set -g fish_pager_color_selected_completion yellow
 set -g fish_pager_color_selected_description FEDC56 # mustard yellow
 set -g fish_pager_color_progress --background=normal
 
-fish_vi_key_bindings
-set -g fish_vi_force_cursor 1
-set -g fish_cursor_default block
-set -g fish_cursor_insert line
-set -g fish_cursor_visual block
-set -g fish_cursor_replace_one underscore
-
 function lla
   eza --all --sort=type --long --git --time-style=relative --smart-group \
     --no-permissions --octal-permissions --color-scale --no-quotes $argv
