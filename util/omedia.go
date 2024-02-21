f8d9663c47e60198444e69e6c3e783b50c5b5233
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-02-21 16:44:32 +0100

    feat(omedia): add writeFile

--- util/omedia.go
+++ util/omedia.go
@@ -2,7 +2,7 @@
  * omedia organizes photo and video files into a directory tree by date
  * Usage: omedia -o <outDir> <inDir>
  * Dependencies
- * - exiv2 extracts date and time from a photo file
+ * - exiv2 extracts date and time from an image file
  * - ffprobe (from ffmpeg) extracts date and time from a video file
  * cp -r ~/Media/sink/2022/* /run/media/vlad/SD1/Media-2021-202_/2022
  */
@@ -35,6 +35,11 @@ func parseArgs() (organizeCommand, error) {
   if len(outDir) == 0 {
     return organizeCommand{}, fmt.Errorf("missing output directory")
   }
+  _, err := os.Stat(outDir)
+  if os.IsNotExist(err) {
+    return organizeCommand{},
+      fmt.Errorf("output directory %v does not exist", outDir)
+  }
   args := flag.Args()
   if len(args) != 1 {
     return organizeCommand{},
@@ -57,18 +62,27 @@ func digestMedia(file string) (string, error) {
   return fmt.Sprintf("%x", hash.Sum(nil)), nil
 }
 
+func writeMedia(file string, tstamp time.Time, digest string) error {
+  dir := tstamp.Format("2006/2006-01-01")
+  file = fmt.Sprintf(
+    "%v_%v%v",
+    tstamp.Format("20060102_150405"), digest[:8],
+    strings.Replace(strings.ToLower(filepath.Ext(file)), ".jpeg", ".jpg", 1),
+  )
+  fmt.Printf("%v/%v\n", dir, file)
+  return nil
+}
+
 type organizer interface {
   organize(outDir string) error
 }
 
-type image struct {
-  file, digest string
-  tstamp time.Time
-}
+type image string
 
 func (img image) organize(outDir string) error {
+  file := string(img)
   out, err := exec.Command(
-    "exiv2", "-K", "Exif.Image.DateTime", "-P", "v", img.file,
+    "exiv2", "-K", "Exif.Image.DateTime", "-P", "v", file,
   ).Output()
   if err != nil {
     return err
@@ -79,26 +93,20 @@ func (img image) organize(outDir string) error {
   if err != nil {
     return err
   }
-  img.tstamp = tstamp
-  digest, err := digestMedia(img.file)
+  digest, err := digestMedia(file)
   if err != nil {
     return err
   }
-  img.digest = digest
-  // fmt.Println(img.tstamp.Format("2006/2006-01-02/20060102_150405"))
-  fmt.Println(img)
-  return nil
+  return writeMedia(file, tstamp, digest)
 }
 
-type video struct {
-  file, digest string
-  tstamp time.Time
-}
+type video string
 
 func (vid video) organize(outDir string) error {
+  file := string(vid)
   out, err := exec.Command(
     "ffprobe", "-v", "quiet", "-select_streams", "v:0", "-show_entries",
-    "stream_tags=creation_time", "-of", "default=nw=1:nk=1", vid.file,
+    "stream_tags=creation_time", "-of", "default=nw=1:nk=1", file,
   ).Output()
   if err != nil {
     return err
@@ -109,15 +117,11 @@ func (vid video) organize(outDir string) error {
   if err != nil {
     return err
   }
-  vid.tstamp = tstamp
-  digest, err := digestMedia(vid.file)
+  digest, err := digestMedia(file)
   if err != nil {
     return err
   }
-  vid.digest = digest
-  // fmt.Println(vid.tstamp.Format("2006/2006-01-02/20060102_150405"))
-  fmt.Println(vid)
-  return nil
+  return writeMedia(file, tstamp, digest)
 }
 
 var (
@@ -136,9 +140,9 @@ func readMedia(inDir string) ([]organizer, error) {
     if (!de.IsDir()) {
       switch {
       case reImage.MatchString(path):
-        media = append(media, image{file: path})
+        media = append(media, image(path))
       case reVideo.MatchString(path):
-        media = append(media, video{file: path})
+        media = append(media, video(path))
       default:
         fmt.Printf("unknown file: %v\n", path)
       }
@@ -154,18 +158,18 @@ func readMedia(inDir string) ([]organizer, error) {
 func main() {
   oc, err := parseArgs()
   if err != nil {
-    fmt.Println(err)
+    fmt.Printf("error: %v\n", err)
     os.Exit(1)
   }
   media, err := readMedia(oc.inDir)
   if err != nil {
-    fmt.Println(err)
+    fmt.Printf("error: %v\n", err)
     os.Exit(1)
   }
   for _, med := range media {
     err := med.organize(oc.outDir)
     if err != nil {
-      fmt.Println(err)
+      fmt.Printf("error: %v\n", err)
     }
   }
 }
